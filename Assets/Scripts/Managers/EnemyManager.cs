e5c3ddcc2a4a7d519562525acaadbe50fe92f600
Daniil <dan.grigulevich@gmail.com>
2022-08-20 16:44:29 +0300

    add unit random spawn

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -1,6 +1,5 @@
 using Assets.Scripts.Models;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Managers
@@ -10,17 +9,41 @@ namespace Assets.Scripts.Managers
         [SerializeField] private Transform[] _spawnPositions;
         [SerializeField] private GameObject[] _unitPrefabs;
 
+        [SerializeField] private float _spawnTime;
+        private float _spawnUnitTime = 0;
+        
+
 
         private void Start()
         {
-            Setup();
+            StartCoroutine(Setup());
         }
 
-        public void Setup()
+        private IEnumerator Setup()
         {
-            for (int i = 0; i < _unitPrefabs.Length; i++)
+            while (true)
             {
-                Instantiate(_unitPrefabs[i], _spawnPositions[i]);
+                if (_spawnUnitTime < _spawnTime)
+                {
+                    yield return new WaitForSeconds(1f);
+                    _spawnUnitTime++;
+                }
+                else
+                {
+                    var unitCount = Random.Range(0, _spawnPositions.Length);
+
+                    for (int i = 0; i < unitCount; i++)
+                    {
+                        var randomPos = Random.Range(0, _spawnPositions.Length);
+                        var randomUnitPrefab = Random.Range(0, _unitPrefabs.Length);
+
+                        Instantiate(_unitPrefabs[randomUnitPrefab], _spawnPositions[randomPos]);
+
+                        yield return new WaitForSeconds(Random.Range(0.5f,1.5f));
+                    }
+
+                    _spawnUnitTime = 0;
+                }
             }
         }
     }
