af7a47523a4e6b3b00cb78e8ba726e8fffb46e70
Daniil <dan.grigulevich@gmail.com>
2022-08-29 21:00:15 +0300

    add main canvas

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+
+    [SerializeField] private Button _startGameButton;
+    [SerializeField] private Button _optionButton;
+    [SerializeField] private Canvas _optionCanvas;
+    [SerializeField] private Canvas _gameCanvas;
+    void Start()
+    {
+        _startGameButton.onClick.AddListener(StartGame);
+        _optionButton.onClick.AddListener(OpenOptionCanvas);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        _gameCanvas.enabled = !_gameCanvas.enabled;
+        _optionCanvas.enabled = true;
+    }
+
+    private void OpenOptionCanvas()
+    {
+
+    }
+}
